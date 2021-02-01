2168c3607a656f27d011f74c2778a6fe8f2f4d99
Nikita Kataev <kaniandr@gmail.com>
2021-02-01 10:54:57 +0300

    [Example] Add example of a test set.

---
+++ tests/da_di/transform/dvmh_sm/dvmh_sm_6/main.c
@@ -0,0 +1,21 @@
+#include "include/main.h"
+
+long A[N][N][2];
+
+int main() {
+  long S = 0;
+  for (int I = 0; I < N; ++I)
+    for (int J = 0; J < N; ++J)
+      for (int K = 0; K < 2; ++K)
+        A[I][J][K] = I + J + K;
+  for (int I = 1; I < N; ++I)
+    for (int J = 1; J < N; ++J)
+      for (int K = 0; K < 2; ++K)
+        A[I][J][K] = A[I - 1][J][K] + A[I][J - 1][K] + A[I][J][K];
+  for (int I = 0; I < N; ++I)
+    for (int J = 0; J < N; ++J)
+      for (int K = 0; K < 2; ++K)
+        S += A[I][J][K];
+  printf("Sum = %ld\n", S);
+  return 0;
+}
