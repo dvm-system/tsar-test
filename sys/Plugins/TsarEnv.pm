8eadb5083ff97384e5bcaa2eac8101c8a8eaea3c
Alexander Smirnov <zoocide@gmail.com>
2021-02-06 09:09:04 +0300

    [System] Fix TsarEnv to propagate all task arguments.

--- sys/Plugins/TsarEnv.pm
+++ sys/Plugins/TsarEnv.pm
@@ -1,6 +1,8 @@
 package Plugins::TsarEnv;
 use strict;
+use Plugins::Base v0.6.1;
 use base qw(Plugins::Base);
+use Task::ID qw(arg2str);
 
 sub on_prepare {
   my $class = shift;
@@ -10,21 +12,19 @@ sub on_prepare {
   my $task_list = shift;
   my $db = shift;
 
-  my $tsar = $task->get_var('', 'tsar');
-  my $platform = $task->get_var('', 'platform');
-  my $clang= $task->get_var('', 'clang');
-  my $include = $task->get_var('', 'include');
-  my $dvm = $task->get_var('', 'dvm', '');
+  my $h = $task->id->args_hash;
+  my @args = sort map {
+    my $g = $_;
+    map { (arg2str($g, $_, @{$h->{$g}{$_}})) } keys %{$h->{$g}}
+  } keys %$h;
+  return if !@args;
+  my $args_str = join ',', @args;
 
   for (my $i = $$pind + 1; $i < @$all_tasks; $i++) {
     my $t = $all_tasks->[$i];
     last if $t->plugin eq 'TsarEnv';
     next if $t->plugin ne 'TsarPlugin';
-    my $new_id = $t->id.($t->id->args ? ',' : ':')."tsar='$tsar'";
-    $new_id .= ",platform=$platform" if $platform;
-    $new_id .= ",clang='$clang'" if $clang;
-    $new_id .= ",include='$include'" if $include;
-    $new_id .= ",dvm='$dvm'" if $dvm;
+    my $new_id = $t->id.($t->id->has_args ? ',' : ':').$args_str;
     $all_tasks->[$i] = $db->new_task($new_id);
   }
 }
