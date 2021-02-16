3c998fb75fc5e00ac9c0afd759fdea9b26b656d9
Alexander Smirnov <zoocide@gmail.com>
2021-02-16 13:02:46 +0300

    [sys] Change sapfor-test.bat to invoke sapfor-test.pl.

--- bin/sapfor-test.bat
+++ bin/sapfor-test.bat
@@ -1,10 +1,6 @@
 @echo off
-set pts=%userprofile%\home\workspace\pts\bin\pts
-set sapfor_test_install_prefix=%~dp0..
-
-if "%sapfor_test_install_prefix%" == "" (echo variable "sapfor_test_install_prefix" is not set & goto :eof)
-if "%pts%" == "" (echo variable "pts" is not set & goto :eof)
-
-set lib=%sapfor_test_install_prefix%\sys
-set sys_tasks=%sapfor_test_install_prefix%\sys\tasks
-call %pts% -I %lib% -T %sys_tasks% --failed=f -s --total-time %*
+::=== Variables set by CMake ===
+set perl=perl
+set lib=%~dp0../sys/lib
+::==============================
+call "%perl%" -I "%lib%" "%~dp0/sapfor-test.pl" %*
