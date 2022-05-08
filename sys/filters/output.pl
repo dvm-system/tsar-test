65e6c28d9e2d2f2e724f6d301fbb059811700bf1
Alexander Smirnov <zoocide@gmail.com>
2022-05-08 18:46:16 +0300

    [sys] Fix filters/output.pl for the git-bash.

--- sys/filters/output.pl
+++ sys/filters/output.pl
@@ -1,4 +1,12 @@
 use Cwd;
 use File::Spec::Functions;
-(my $s = canonpath(cwd)) =~ s~\\~\\\\~g;
+BEGIN {
+  ($s = canonpath(cwd)) =~ s~\\~\\\\~g;
+  if($^O eq 'msys') {
+    ## change the path '/c/Users/user/path' to 'C:\\Users\\user\\path' ##
+    my @d = split /\//, $s;
+    shift @d; #< remote the first '/'
+    $s = join '\\\\', (uc shift @d).':', @d;
+  }
+}
 s~$s[\\/]~~g;
